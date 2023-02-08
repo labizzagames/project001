3fb2ef8bb3a7ddc94b2869a3bc78925315a4fa97
StefanoCld <caldana.stefano@hotmail.it>
2023-02-08 23:19:30 +0100

    First draft of a local sea area (with cameras)

---
+++ Assets/SimpleMovingScript.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+
+public class SimpleMovingScript : MonoBehaviour
+{
+    public CinemachineVirtualCamera CMFishing;
+
+    public AudioSource audioSource;
+
+    public AudioClip cameraInSound;
+    public AudioClip cameraOutSound;
+
+    public bool bIsFishing = false;
+
+    public float zMovingMult;
+    public float zInterpFactor = 0.4f;
+    public float rotMovingMult;
+
+    private void Move()
+    {
+        float xInput = Input.GetAxis("Horizontal");
+        float zInput = Input.GetAxis("Vertical");
+
+        zInput = Mathf.Clamp(zInput, 0.0f, 1.0f);
+
+        this.transform.position = Vector3.Lerp
+            (
+            this.transform.position,
+            this.transform.position + (this.transform.forward) * zInput * zMovingMult * Time.deltaTime,
+            zInterpFactor
+            );
+
+        this.transform.Rotate(Vector3.up, rotMovingMult * xInput * Time.deltaTime);
+    }
+
+    private void Fish()
+    {
+        float xInput = Input.GetAxis("Horizontal");
+
+        this.transform.Rotate(Vector3.up, rotMovingMult * xInput * Time.deltaTime);
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            bIsFishing = !bIsFishing;
+
+            if (bIsFishing)
+            {
+                audioSource.clip = cameraInSound;
+                audioSource.Play();
+                CMFishing.Priority = 2;
+            }
+            else
+            {
+                audioSource.clip = cameraOutSound;
+                audioSource.Play();
+                CMFishing.Priority = 0;
+            }
+        }
+
+        if (!bIsFishing)
+            Move();
+        else
+            Fish();
+    }
+}
